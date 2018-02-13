4fbce27a4e196f04d1d8fd784a7f3bdc9fda0574
Ryan C <031247ryan@gmail.com>
2018-02-13 11:10:22 -0800

    added data pull from yelp

--- src/app/foodsearch/foodsearch.component.ts
+++ src/app/foodsearch/foodsearch.component.ts
@@ -14,17 +14,35 @@ import {HttpParams } from '@angular/common/http';
 export class FoodsearchComponent implements OnInit {
   address = '';
   token = 'GKUmYzkZ-5Mvc_rp8eiGtXSNDugMAs_lF54n5SfWsWmudDvqg1dOb3Bi5sLbgykBH85K6WSvQrnOmywcnMfgkhMjwduHpjGNz6L1EaIani-USmFoVNJcbU2yT0iCWnYx';
+
+  businessName: string[] = [];
+  businessAddress: string[] = [];
+  businessImage: string[] = [];
+  businessURL: string[] = [];
+  businessLat: string[] = [];
+  businessLong: string[] = [];
+
   constructor(private LocationServiceService: LocationServiceService, private http: HttpClient) { }
 
   ngOnInit() {
     this.address = this.LocationServiceService.formatted_address;
 
-    let url="https://api.yelp.com/v3/businesses/search?location=Vancouver&term=sushi&limit=50";
-    let header =new HttpHeaders({'Content-Type': 'application/json', 'Authorization': 
-        'Bearer '+ this.token});
+    let url="http://www.youads.co/php/1.php";
 
-    this.http.get(url, { headers: new HttpHeaders().set('Authorization', 'Bearer ' + this.token) }).subscribe(data => {
+    this.http.get(url, { }).subscribe(data => {
       console.log(data);
+      this.storeAPIData(data);
+    });
+  }
+
+  storeAPIData(data) {
+    data.businesses.forEach(element => {
+      this.businessName.push(element.name);
+      this.businessAddress.push(element.location.address1); 
+      this.businessLat.push(element.coordinates.latitude);
+      this.businessLong.push(element.coordinates.longitude);
+      this.businessImage.push(element.image_url);
+      this.businessURL.push(element.url);
     });
   }
 }
